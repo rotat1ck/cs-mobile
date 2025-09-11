6b8e5294d3824ee968a1da6f0fb8ba56564d1610
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-09-11 18:48:43 +0300

    Properties, entries, images - 11.09

---
+++ classwork/2025.09/control-elements2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace control_elements2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ classwork/2025.09/control-elements2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace control_elements2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e) {
+           /// label1.Text = entry1.Text;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
+            label1.TextColor = Color.FromRgba(0, 255, 255, 0.8);
+        }
+    }
+}
