68c96eb875898e40efa2bd0f512530302e5850e8
rotat1ck <rotatick32151@gmail.com>
2025-09-12 00:15:59 +0300

    Practice#1 - Mid-save#2

--- homework/2025.09/21.09 practice#1/MauiProgram.cs
+++ homework/2025.09/21.09 practice#1/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace practice1
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("MonumentExtended-Regular.ttf", "MonumentExtended");
                 });
 
 #if DEBUG
