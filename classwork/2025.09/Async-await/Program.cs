8f8dd9f472a6d053d77c3e5df5b0ae36466bef63
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-09-18 19:04:53 +0300

    Socket server - 18.09

---
+++ classwork/2025.09/Async-await/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Async_await;
+    
+internal class Program {
+    static void Main(string[] args) {
+        Server s = new Server("127.0.0.1", 1024);
+        s.Start();
+        Console.Read();
+        s.Stop();
+    }
+}
+
---
+++ classwork/2025.09/Async-await/Server.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Async_await;
+
+internal class Server {
+    delegate void ConnectDelegate(Socket s);
+    delegate void StartNetwork(Socket s);
+    Socket? socket;
+    IPEndPoint endP;
+
+    public Server(string strArr, int port) {
+        endP = new IPEndPoint(IPAddress.Parse(strArr), port);
+    }
+
+    async Task Server_Connect(Socket s) {
+        await s.SendAsync(Encoding.ASCII.GetBytes(DateTime.Now.ToString()));
+        s.Shutdown(SocketShutdown.Both);
+        s.Close();
+    }
+
+    async Task Server_Begin(Socket s) {
+        while (true) {
+            try {
+                while (s != null) {
+                    var ns = await s.AcceptAsync();
+                    Console.WriteLine($"{ns.RemoteEndPoint} connected");
+                    await Server_Connect(ns);
+                }
+            } catch (SocketException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public async void Start() {
+        if (socket != null) {
+            return;
+        }
+
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(endP);
+        socket.Listen(10);
+        await Server_Begin(socket);
+    }
+
+    public void Stop() {
+        if (socket != null) {
+            try {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                socket = null;
+            } catch (SocketException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
