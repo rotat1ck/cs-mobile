11e9e34fb46bcd4c88898305e1255e664fe2f57f
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-09-23 19:45:04 +0300

    Datepicker, as, is - 23.09

---
+++ classwork/2025.09/datepicker, timepicker/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace datepicker__timepicker
+{
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        void DateSelected(object sender, DateChangedEventArgs e) {
+            if (label is { })
+                label.Text = $"Вы выбрали {e.NewDate.ToString("d")}";
+        }
+    }
+}
---
+++ classwork/2025.09/datepicker, timepicker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace datepicker__timepicker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
