a8d6778d43cabe6f9ab5fcbe569603ffac1366bc
rotat1ck <rotatick32151@gmail.com>
2025-10-06 18:12:15 +0300

    Webview, alerts, progressbar - 06.10

---
+++ classwork/2025.10/webview, alerts, progressbar/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace webview
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ classwork/2025.10/webview, alerts, progressbar/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "webview")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "webview.Pages")]
