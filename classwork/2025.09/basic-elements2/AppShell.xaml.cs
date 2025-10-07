84768bb7ae133dea9a7e30b263594af81c2581f8
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-10-07 12:50:57 +0300

    Switch, picker, radiobuttons

---
+++ classwork/2025.09/basic-elements2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace basic_elements2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ classwork/2025.09/basic-elements2/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace basic_elements2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void statusCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e) {
+            statusLabel.Text = $"Статус: {(e.Value ? "женат/замужем" : "холост/не замужем")}";
+        }
+
+        private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e) {
+            RadioButton selectedButton = (RadioButton)sender;
+            if (header != null) {
+                header.Text = $"Выбранный язык: {selectedButton.Content}";
+            }
+        }
+
+        private void langPicker_SelectedIndexChanged(object sender, EventArgs e) {
+            header.Text = $"Вы выбрали путь: {langPicker.SelectedItem}";
+        }
+    }
+
+}
