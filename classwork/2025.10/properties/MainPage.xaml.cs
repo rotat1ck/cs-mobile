ffbb6243255cf2c12c6e0ec8a2ee3ed2a3ddff07
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-10-17 17:29:00 +0300

    Properties - 17.10

---
+++ classwork/2025.10/properties/MainPage.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace properties {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ classwork/2025.10/properties/StringToStyleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties {
+    internal class StringToStyleConverter : IValueConverter {
+        public Style ApprovedStatus { get; set; }
+        public Style DeniedStatus { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string username = (string)value;
+            if (username == "admin") return ApprovedStatus;
+            return DeniedStatus;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            var status = value as Style;
+
+            if (status != ApprovedStatus) {
+                return "admin";
+            }
+
+            return "user";
+        }
+    }
+}
