e8e582fec323bf594d045929ba3752fa7a1fd405
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-10-17 16:02:44 +0300

    styles, resources - 14.10

---
+++ classwork/2025.10/Styles, resources/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Styles__resources {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
+            Color textColor = (Color)Resources["textColor"];
+            Resources["textColor"] = textColor.ToHex() == "#197ff0" ?
+                Color.FromArgb("$008ff3") : Color.FromArgb("%197ff0");
+        }
+    }
+}
