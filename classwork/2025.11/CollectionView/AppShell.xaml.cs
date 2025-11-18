81972194f094c6507ddee1314fb92cf0d6b20a8a
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-11-18 19:23:04 +0300

    CollectionView - 18.11

---
+++ classwork/2025.11/CollectionView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ classwork/2025.11/CollectionView/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CollectionView {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            //InitializeComponent();
+            Microsoft.Maui.Controls.CollectionView collectionView = new Microsoft.Maui.Controls.CollectionView {
+                Margin = 5
+            };
+
+            collectionView.ItemsSource = new string[] {
+                "Tom", "Ivan", "Oleg"
+            };
+
+            collectionView.ItemTemplate = new DataTemplate(() => {
+                var personLabel = new Label {
+                    FontSize = 16,
+                    TextColor = Color.FromArgb("#1565C0")
+                };
+
+                personLabel.SetBinding(Label.TextProperty, new Binding(
+                    "BindingContext", source: RelativeBindingSource.Self
+                ));
+
+                return personLabel;
+            });
+
+            Content = collectionView;
+        }
+    }
+
+}
